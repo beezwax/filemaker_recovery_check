b01e5e011c83951369b3e5868bc147ca575230e1
Victor Olivo <volivo@apple.com>
2025-06-23 17:43:21 +0000

    adding checks for corruption

--- filemaker_recovery_check.py
+++ filemaker_recovery_check.py
@@ -5,7 +5,7 @@
 #   Created: 2025-06-03 Simon Brown
 
 
-import argparse, os, sys, subprocess
+import argparse, os, sys, subprocess, re
 from pprint import pprint
 
 # USAGE
@@ -15,6 +15,22 @@ FIND_COMMAND = '/usr/bin/find'
 FMDT_COMMAND = 'FMDeveloperTool'
 
 ARGPARSER: argparse = None
+
+#
+# list of corrupted file patterns to check file
+#
+CORRUPTED_FILE_PATTERNS = [
+    "This item changed$",
+    "Adjusted item count of library .* \(.*\) by .*",
+    "Calculation modified$",
+    "Dropped page starts with data of type",
+    "ERROR: copy or rebuild of file blocks failed; try another option in Advanced Recover Options",
+    "Found .* stranded library object\(s\)",
+    "File blocks: scanned and rebuilt .* block\(s\), dropped ([^0]) invalid data",
+    "Item count changed from .* to .*",
+    "Rebuilt value index for field .* old index is ([^0])",
+    "Recovered .* stranded library object\(s\) for a total of .* byte\(s\)",
+]
 
 
 def find_files (parent_dir: str, file_pattern: str) -> list:
@@ -41,10 +57,12 @@ def find_files (parent_dir: str, file_pattern: str) -> list:
         parent_dir,
         '-maxdepth', '2',
         '-name', file_pattern],
-        capture_output=True
+        capture_output=True,
+        text=True,
     )
-
-    return output.stdout.decode("utf-8").split('\n')
+    # filter out the empty string that split generates
+    found_files = list(filter(None, output.stdout.split('\n')))
+    return found_files
 
 
 def find_newest_dir (parent_dir: str) -> str:
@@ -125,6 +143,7 @@ def recover_file (file_path: str, parent_dir: str, passphrase: str) -> bool:
     file_dir = os.path.dirname (file_path)
     file_ext = file_name.split('.')[-1]
 
+    # ignore files without the fmp12 extension
     if file_ext != 'fmp12':
         print(file_ext)
         print ('Warning: "' + file_name + '"' + ' may not be a FileMaker file and is being skipped' )
@@ -133,46 +152,76 @@ def recover_file (file_path: str, parent_dir: str, passphrase: str) -> bool:
     # TODO: use RE here instead so we only replace at end of string.
     file_path_out = file_path.replace ('.fmp12', '_recovered.fmp12', 1)
 
-    #try:
-    #   os.remove (file_dir + "Recover.log")
-    #except OSError as e:
-    #   pass
 
-    if passphrase == None:
-        result = subprocess.run (
-            [FMDT_COMMAND,
-            parent_dir,
-            '--recover', file_path,
-            '-target_filename', file_path_out],
-            capture_output=True
-        )
-    else:
-        # Passphrase will be ignored for any files that don't need it.
-        result = subprocess.run (
-            [FMDT_COMMAND,
-            parent_dir,
-            '--recover', file_path,
-            '-target_filename', file_path_out,
-            '-encryption_key', passphrase],
-            capture_output=True
-        )
+    # build fm recovery command
+    fm_data_recovery_command = [
+        FMDT_COMMAND,
+        parent_dir,
+        '--recover', f"{file_path}",
+        '-target_filename', f"{file_path_out}",
+    ]
+    # append passphrase if we got one
+    if passphrase != None:
+        fm_data_recovery_command.append('--passphrase', passphrase)
 
-    #
-    # collect output
-    #
-    output_lines = result.stdout.splitlines()
-    for line in output_lines:
-        print(line)
+    # set the recovery log file
+    recovery_log_file = f'{parent_dir}/Recover.log'
 
+    # run command
     try:
+        print(f"Attempting to perform file recovery for {file_name}.\nRecovery file: {file_path_out}")
+        result = subprocess.run (
+            fm_data_recovery_command,
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with exit code: {e.returncode}")
+        print(f"Stdout: {e.stdout}")
+        print(f"Stderr: {e.stderr}")
+
+    # output log file
+    print(f"Recovery Log File: {recovery_log_file}")
+
+    # remove recovery file
+    try:
+        print(f"Removing recovery file at {file_path_out}")
         os.remove (file_path_out)
     except OSError as e:
         print ('Error: no output file created for "' + file_name + '"')
         return False
 
-    if output.returncode == 0:
-        # NO ACTION CURRENTLY
-        pass
+    if result.returncode == 0:
+        return {
+            "file_name": file_name,
+            "file_path": file_path,
+            "recovery_log_file": recovery_log_file
+        }
+
+#
+# parse files for corruptions
+#
+def check_file_corruption(file_path: str, pattern: str):
+    """
+    Searches for a pattern for fms file corruption
+
+    Args:
+        file_path (str): The path to the file to search.
+        pattern (str): The regular expression pattern to search for.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            # Compile the regular expression for efficiency if used repeatedly
+            compiled_pattern = re.compile(pattern)
+            for line_number, line in enumerate(file, 1):
+                if compiled_pattern.search(line):
+                    print(f"Line {line_number}: {line.strip()}")
+    except FileNotFoundError:
+        print(f"Error: File not found at {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 #
 #   M A I N
@@ -186,10 +235,9 @@ if __name__ == "__main__":
 
     arguments = ARGPARSER.parse_args()
 
-    print (arguments.path)
-    print (arguments.filepattern)
-    print (arguments.newest)
-    print (arguments.passphrase)
+    #print (arguments.path)
+    #print (arguments.filepattern)
+    #print (arguments.newest)
 
     path = arguments.path
 
@@ -206,16 +254,39 @@ if __name__ == "__main__":
 
     # Currently, remove an previous log. Instead, we'll accumulate the messages for
     # all files. Later versions of this script we'll scan the logs after each recovery.
-
     try:
         os.remove (path + "Recover.log")
     except OSError as e:
         pass
 
-
+    # perform file recovery - remove recovered file, keep log file
+    recovered_file_logs = []
     for file in file_list:
-        if recover_file (file, path, arguments.passphrase) == False:
+        recover_file_result = recover_file (file, path, arguments.passphrase)
+        if recover_file_result == False:
             return_code = -2
             break
+        recovered_file_logs.append(recover_file_result)
 
+    # check through log files
+    for obj in recovered_file_logs:
+        # collect data
+        corrupted_checks = []
+        file_name = obj['file_name']
+        file_path = obj['file_path']
+        recovery_log_file = obj['recovery_log_file']
+
+        # loop through patterns to check
+        print(f"Reviewing recovery log for {file_path}")
+        for pattern in CORRUPTED_FILE_PATTERNS:
+            result = check_file_corruption(recovery_log_file, pattern)
+            if result != None:
+                print(f"Found possible corruption for {file_path}. Triggered by the search pattern, {pattern}")
+                corrupted_checks.append(file_name)
+
+        # alert if we found nothing
+        if not corrupted_checks:
+            print(f"No file corruption found for {file_path}")
+
+    # DONE
     sys.exit (return_code)
